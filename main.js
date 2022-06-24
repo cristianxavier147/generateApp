17ff538ed73a02ed11b65e029a7b40b1b0e6bb98
cristian <cristianlopezsarango@gmail.com>
2022-06-24 13:55:09 -0500

    click button and styles

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-let id = document.querySelector(".id");
-let p = document.querySelector(".p");
+let id = document.getElementById("title");
+let p = document.getElementById("text");
 
 //consumo de api
-window.addEventListener("DOMContentLoaded", (e) => {
-  e.preventDefault();
-  fetch("https://api.adviceslip.com/advice")
-    .then((response) => response.json())
-    .then(function (data) {
-      id.innerHTML = `Advice # ${data.slip.id}`;
-      p.innerHTML = `"${data.slip.advice}"`;
-    });
+// fetch("https://api.adviceslip.com/advice")
+//   .then((response) => response.json())
+//   .then(function (data) {
+//     id.innerHTML = `Advice # ${data.slip.id}`;
+//     p.innerHTML = `"${data.slip.advice}"`;
+//   });
+
+document.querySelector(".ver").addEventListener("click", function () {
+  const consultar = async () => {
+    const resultado = await fetch("https://api.adviceslip.com/advice");
+    const data = await resultado.json();
+    id.innerHTML = `Advice #${data.slip.id}`;
+    p.innerText = `"${data.slip.advice}"`;
+  };
+  consultar();
 });
